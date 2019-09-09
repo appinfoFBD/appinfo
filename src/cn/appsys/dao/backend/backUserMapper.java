926a448bf6a0b4eaf2ec13ee61b7c10281634a01
hfh1458577278 <1458577278@qq.com>
2019-09-09 16:06:19 +0800

    提交

--- src/cn/appsys/dao/backend/backUserMapper.java
+++ src/cn/appsys/dao/backend/backUserMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import cn.appsys.pojo.backend_user;
 
 public interface backUserMapper {
+	
     //登录
 	public backend_user doLoing(@Param("userCode")String userName);
 }
